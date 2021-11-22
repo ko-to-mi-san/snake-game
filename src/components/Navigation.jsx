854b974db20d64463d9fdc0ddceb2d40de7e84ad
Kotomi <piitun3232@gmail.com>
2021-11-22 21:48:30 +0900

    修正

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navigation = ({ length, difficulty = 3, onChangeDifficulty }) => {
   const upVisibility = difficulty < 5 ? '' : 'is-hidden'
-  const downVisibility = difficulty > 1 ? '' : 'is-hidden'
-  const onUpDifficulty = () => onChangeDifficulty(difficulty + 1)
-  const onDownDifficulty = () => onChangeDifficulty(difficulty - 1)
+   const downVisibility = difficulty > 1 ? '' : 'is-hidden'
+   const onUpDifficulty = () => onChangeDifficulty(difficulty + 1)
+   const onDownDifficulty = () => onChangeDifficulty(difficulty -1)
   return (
     <div className="navigation">
       <div className="navigation-item">
