72ec813c235c0333080e03cb7feb23501df63a88
Kotomi <piitun3232@gmail.com>
2021-12-26 15:12:09 +0900

    コンポーネント追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Navigation from './components/Navigation'
+import Field from './components/Field'
+import Button from './components/Button'
+import ManipulationPanel from './components/ManipulationPanel'
 import './App.css';
 
 function App() {
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Field = () => {
   return (
     <div className="field">
-      <p>dield</p>
+      <p>field</p>
     </div>
   );
 };
