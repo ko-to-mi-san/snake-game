17261e22fb0478d5672fc09b9fb844efa8bcdcb6
Kotomi <piitun3232@gmail.com>
2022-01-01 23:30:10 +0900

    ボタン操作できるようにする

--- src/App.js
+++ src/App.js
@@ -30,6 +30,13 @@ const initialValues = initFields(35, initialPosition)
       down: 'up'
     })
 
+    const Delta = Object.freeze({
+      up: { x: 0, y: -1 },
+      right: { x: 1, y: 0 },
+      left: { x: -1, y: 0 },
+      down: { x: 0, y: 1 },
+    })
+
   let timer = undefined
 
   const unsubscribe = () => {
@@ -69,7 +76,7 @@ function App() {
     if (!position || status !== GameStatus.playing) {
       return
     }
-    const canContinue = goUp()
+    const canContinue = handleMoving()
      if (!canContinue) {
        setStatus(GameStatus.gameover)
      }
@@ -97,15 +104,19 @@ function App() {
     setDirection(newDirection)
   }
 
-  const goUp = () => {
+  const handleMoving = () => {
     const { x,y } = position
-    const newPosition = { x, y: y -1 }
+    const delta = Delta[direction]
+    const newPosition = {
+      x: x + delta.x,
+      y: y + delta.y
+    }
      if (isCollision(fields.length, newPosition)) {
        unsubscribe()
        return false
      }
     fields[y][x] = ''
-    fields[newPosition.y][x] = 'snake'
+    fields[newPosition.y][newPosition.x] = 'snake'
     setPosition(newPosition)
     setFields(fields)
     return true
