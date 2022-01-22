2534cb018d993001bab6fd42b267e8689ef10196
Kotomi <piitun3232@gmail.com>
2022-01-22 23:08:37 +0900

    ロジック切り出し

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,3 +23,18 @@ fields[food.y][food.x] = 'food'
 
   return fields
 }
+
+export const isCollision = (fieldSize, position) => {
+  if (position.y < 0 || position.x < 0) {
+    return true;
+  }
+  if (position.y > fieldSize -1 || position.x > fieldSize -1) {
+    return true;
+  }
+
+  return false;
+}
+
+export const isEatingMyself = (fields, position) => {
+  return fields[position.y][position.x] === 'snake'
+}
